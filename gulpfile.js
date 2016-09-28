a6c3138c0ebdbb96cfe665eb7cc4a6af26ada9c8
Yulia Garifulina <hackwrenchh@gmail.com>
2016-09-28 15:56:50 +0300

    Initial state of project

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const gulp = require('gulp');
-// var sass = require('gulp-sass');
+var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 // var useref = require('gulp-useref');
 // var uglify = require('gulp-uglify');
@@ -17,11 +17,11 @@ var runSequence = require('run-sequence');
 // var svgSprite = require('gulp-svg-sprite');
 var config = {
     "mode": {
-    css: { // Create a «css» sprite
-        render: {
-            scss: true // Render a Sass stylesheet
-        }
-      },
+        css: { // Create a «css» sprite
+            render: {
+                scss: true // Render a Sass stylesheet
+            }
+        },
         "defs": true,
         "symbol": true
     }
@@ -29,61 +29,66 @@ var config = {
 // var $ = require('gulp-load-plugins')();
 
 var $ = require('gulp-load-plugins')({
-            pattern: ['gulp-*', 'gulp.*'],
-            replaceString: /\bgulp[\-.]/,
-            lazy: true,
-            camelize: true
-          });
+    pattern: ['gulp-*', 'gulp.*'],
+    replaceString: /\bgulp[\-.]/,
+    lazy: true,
+    camelize: true
+});
 
-gulp.task('sass', function () {
-  return gulp.src('app/scss/**/*.scss')
-      .pipe($.sourcemaps.init())
-      .pipe($.sass())
-      .pipe($.autoprefixer({
-          browsers: ['last 3 versions'],
-          cascade: false
-      }))
-      .pipe($.sourcemaps.write('./maps'))
-      .pipe(gulp.dest('app/css'))
-      .pipe(browserSync.reload({
-        stream: true
-      }))
+gulp.task('sass', function() {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass({
+            // includePaths: require('node-normalize-scss').with('other/path', 'another/path')
+            // - or -
+            includePaths: require('node-normalize-scss').includePaths
+        }))
+        .pipe($.sourcemaps.init())
+        .pipe($.sass())
+        .pipe($.autoprefixer({
+            browsers: ['last 3 versions'],
+            cascade: false
+        }))
+        .pipe($.sourcemaps.write('./maps'))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
 
-gulp.task('watch', ['browserSync', 'sass'], function () {
-  gulp.watch('app/scss/**/*.scss', ['sass']);
-  gulp.watch('app/*.html', browserSync.reload);
-  gulp.watch('app/js/**/*.js', browserSync.reload);
+gulp.task('watch', ['browserSync', 'sass'], function() {
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
 });
 
 
-gulp.task('browserSync', function () {
-  browserSync.init({
-    server: {
-      baseDir: 'app'
-    }
-  })
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        }
+    })
 });
 
 
-gulp.task('useref', function () {
-  return gulp.src('app/*.html')
-      .pipe($.useref())
-      // Minifies only if it's a JavaScript file
-      .pipe($.gulpIf('*.js', $.uglify()))
-      // Minifies only if it's a CSS file
-      .pipe($.gulpIf('*.css', $.cssnano()))
-      .pipe(gulp.dest('dist'))
+gulp.task('useref', function() {
+    return gulp.src('app/*.html')
+        .pipe($.useref())
+        // Minifies only if it's a JavaScript file
+        .pipe($.gulpIf('*.js', $.uglify()))
+        // Minifies only if it's a CSS file
+        .pipe($.gulpIf('*.css', $.cssnano()))
+        .pipe(gulp.dest('dist'))
 });
 
 
-gulp.task('images', function () {
-  return gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
-      .pipe($.cache($.imagemin({
-        interlaced: true
-      })))
-      .pipe(gulp.dest('dist/images'))
+gulp.task('images', function() {
+    return gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
+        .pipe($.cache($.imagemin({
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'))
 });
 
 
@@ -98,12 +103,12 @@ gulp.task('clean:dist', function() {
 });
 
 
-gulp.task('cache:clear', function (callback) {
+gulp.task('cache:clear', function(callback) {
     return $.cache.clearAll(callback)
 });
 
 
-gulp.task('build', function (callback) {
+gulp.task('build', function(callback) {
     $.runSequence('clean:dist',
         'sass', 'useref', ['images', 'fonts'],
         callback
@@ -111,35 +116,35 @@ gulp.task('build', function (callback) {
 });
 
 
-gulp.task('default', function (callback) {
-    runSequence(['sass','browserSync', 'watch'],
+gulp.task('default', function(callback) {
+    runSequence(['sass', 'browserSync', 'watch'],
         callback
     )
 });
 
 
 // generate sprite.png and _sprite.scss
-gulp.task('sprites', function () {
+gulp.task('sprites', function() {
     return $.sprity.src({
-        src: './img/sprite/**/*.{png,jpg}',
-        style: './sprite.css',
-        // ... other optional options
-        // for example if you want to generate scss instead of css
-        //processor: 'sass', // make sure you have installed sprity-sass
-    })
+            src: './img/sprite/**/*.{png,jpg}',
+            style: './sprite.css',
+            // ... other optional options
+            // for example if you want to generate scss instead of css
+            //processor: 'sass', // make sure you have installed sprity-sass
+        })
         .pipe(gulpif('*.png', gulp.dest('./dist/img/'), gulp.dest('./dist/css/sprite')))
 });
 
 
-gulp.task('svg-sprites', function () {
-  return gulp.src('sprite/*.svg')
-      .pipe($.svgSprite(config))
-      .pipe(gulp.dest('app/img'))
+gulp.task('svg-sprites', function() {
+    return gulp.src('sprite/*.svg')
+        .pipe($.svgSprite(config))
+        .pipe(gulp.dest('app/img'))
 });
 
 
 gulp.task('lint', () =>
-  gulp.src('app/scripts/**/*.js')
+    gulp.src('app/scripts/**/*.js')
     .pipe($.eslint())
     .pipe($.eslint.format())
     .pipe($.if(!$.browserSync.active, $.eslint.failOnError()))
