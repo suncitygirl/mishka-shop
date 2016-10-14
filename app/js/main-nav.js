057f5ae48a682458e51896e811a4a101a6545b71
Yulia Garifulina <hackwrenchh@gmail.com>
2016-10-15 02:59:02 +0300

    Add transformation of open/close button in the main navigation

--- app/js/main-nav.js
+++ app/js/main-nav.js
@@ -3,6 +3,7 @@ var openMenu = document.getElementsByClassName('main-nav__open-btn')[0];
 function showMobileMenu(event) {
     event.preventDefault();
     var mobileMenu = document.getElementsByClassName('main-nav__dropdown-list')[0];
+    openMenu.classList.toggle('main-nav__close-btn');
     mobileMenu.classList.toggle('main-nav__dropdown-list--show');
 }
 openMenu.addEventListener('click', showMobileMenu);
